d6f3202884a4e6387e9bcc060d56423a4857c8b1
Tom Kuson <mail@tjkuson.me>
2025-06-22 14:55:04 +0100

    Setup settings

---
+++ src/tabbit/config/settings.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import final
+
+from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict
+
+
+@final
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="tabbit_", env_file=".env")
+
+    log_filename: Path = Path("tabbit.log.jsonl")
+
+
+settings = Settings()
